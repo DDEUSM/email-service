ae44d4a54fd3d0690bbf27f5e6f54bec4c48270c
David de Deus Mesquita <72158476+DDEUSM@users.noreply.github.com>
2024-02-28 17:31:10 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker compose up -d
 
-PGPASSWORD=1234 psql -h localhost -U postgres -c "CREATE DATABASE emailservice"
+PGPASSWORD=1234 psql -h "0.0.0.0" -U postgres -c "CREATE DATABASE emailservice"
 
 docker exec -it nodejs npm run test 
