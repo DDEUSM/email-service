024bcc16586bde1b032240c1145d9ac05b76b5b8
david de deus <daviddeusmesquita@gmail.com>
2024-05-15 17:38:06 -0300

    Fix the Generate query class ---------------------------- - Fix the static methods such as, insert, query, delete and update of GenerateQuery class - Because the old implementation have been a lot of problems to perform right

--- src/utils/construct-query.ts
+++ src/utils/construct-query.ts
@@ -1,53 +1,43 @@
-import { getRandomValues } from "crypto"
-import { opendir } from "fs"
 
 export class GenerateQuery
 {
-    private static formatData(data: any): {values: any, keys: string[], finalIndex: number}
-    {
-        const values = Object.values(data)
-        const keys = Object.keys(data)    
-        const finalIndex = values.length - 1
-        return { values, keys, finalIndex }
-    }
+    public static insert (tableName: string, properties: string[]): string
+    { 
+        const finalIndex = properties.length - 1
 
-    public static insert (tableName: string, data: any): string
-    {
-        const { values, keys, finalIndex } = GenerateQuery.formatData(data)
-
-        return values.reduce((query, current, index) => {
+        return properties.reduce((query: string, key: string, index: number) => {
             return index === finalIndex? 
                 query += "$"+(index+1)+");"
                 : query += "$"+(index+1)+","            
-        }, `INSERT INTO ${tableName} (${keys}) VALUES (`) as string
+        }, `INSERT INTO ${tableName} (${properties}) VALUES (`) as string
     }
 
-    public static query (tableName: string, data: any, offset?: number, limit?: number): string
+    public static query (tableName: string, properties: string[], offset?: number, limit?: number): string
     {
-        const { values, keys, finalIndex } = GenerateQuery.formatData(data)
+        const finalIndex = properties.length - 1
 
-        return values.reduce((query: string, current: any, index: number) => {
+        return properties.reduce((query: string, key: string, index: number) => {
             if (index === finalIndex)
             {
-                query += `${keys[index]} = ${typeof offset === 'number'? 
+                query += `${key} = ${typeof offset === 'number'? 
                     (`$${index+1} OFFSET ${offset} LIMIT ${limit};`) 
                     : 
                     (`$${index+1};`)}`
                 return query  
             } 
-            query += `${keys[index]} = $${index+1},`
+            query += `${key} = $${index+1} AND `
             return query                         
         }, `SELECT * FROM ${tableName} WHERE `) as string
     }
 
-    public static updateOne (tableName: string, id: string, updatedData: any): string
-    {
-        const { values, keys, finalIndex } = GenerateQuery.formatData(updatedData)
+    public static updateOne (tableName: string, id: string, properties: string[]): string
+    {    
+        const finalIndex = properties.length - 1
 
-        return values.reduce((query, current, index) => {
+        return properties.reduce((query: string, key: string, index: number) => {
             return index === finalIndex? 
-                query += `${keys[index]} = $${(index+1)} WHERE id=$${(index+2)};`
-                : query += `${keys[index]} = $${(index+1)},`            
+                query += `${key} = $${(index+1)} WHERE id='${id}';`
+                : query += `${key} = $${(index+1)},`            
         }, `UPDATE ${tableName} SET `) as string
     }
 
